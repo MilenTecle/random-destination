549a61eb1772779ac5a9bb0c6fe31fcb438e622a
Milen Tecle <m_tecle@hotmail.com>
2023-11-02 13:29:33 +0000

    Add a while loop to the first function, user_input

--- run.py
+++ run.py
@@ -1,13 +1,25 @@
 
 def user_input():
+    """
+    Get input from user and to make sure user is making a valid 
+    choice. The while loop will repeat and request data that is valid
+    """
+    while True:
+        user_choice = input("Are you traveling alone or with company? (alone/with company):\n")
+        if user_choice == "alone":
+            return "alone"
+        elif choice == "with company":
+            return "with company"
+        else:
+            print("Invalid choice. Please choose 'alone' or 'with company'.")
 
 
 
-def travel_dates():
+#def travel_dates():
 
 
 
-def duration():
+#def duration():
 
 
 def main():
