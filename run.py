8e0778e14fa675b10159428c4cf6690ca40579ec
Milen Tecle <m_tecle@hotmail.com>
2023-11-03 12:45:33 +0000

    Tweak the user_choice function and add data to the duration function

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import datetime
+from  datetime import datetime
 import random
 
 
@@ -8,13 +8,13 @@ def user_input():
     choice. The while loop will repeat and request data that is valid
     """
     while True:
-        user_choice = input("Are you traveling alone or with company? (alone/with company):\n")
+        user_choice = input("Are you traveling alone or with company? (alone/with company):\n").strip()
         if user_choice == "alone":
             return "alone"
-        elif choice == "with company":
+        elif user_choice == "with company":
             return "with company"
         else:
-            print("Invalid choice. Please choose 'alone' or 'with company'.")
+            print("Invalid choice. Please enter 'alone' or 'with company'.")
 
 
 
@@ -22,13 +22,22 @@ def travel_date():
     """
     Get input from the user about travel date
     """
-    departure = input("When do you want to travel? (MM/DD/YYY):\n")
-    return departure
+    while True:
+        departure = input("When do you want to travel? (YYYY-MM-DD):\n")
+        try:
+            day_and_date = f"{datetime.now().strftime('%A')}, {datetime.now().date().strftime('%Y-%M-%D')}"
+            return day_and_date
+        except ValueError:
+            print("Invalid date format. Please enter the date in YYYY-MM-DD format")
+    
 
 
-
-
-#def duration():
+def duration():
+    """
+    Get input from the user about the duration of the stay
+    """
+    travel_duration = int(input("How many days are you planning to stay?:\n"))
+    return duration
 
 
 def main():
@@ -36,6 +45,7 @@ def main():
     
     user_choice = user_input()
     departure = travel_date()
+    travel_duration = duration()
 
 
 
